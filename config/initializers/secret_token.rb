d69af4815dc87cd87bd58b6785efba4b4ebbb0e1
Bryan Marble <bryan@mill33.com>
2012-12-27 16:02:20 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+M33::Application.config.secret_token = 'f68f2c85896450f80921d4e8ca7c16cfb0abb9c2fa612c8cab903615efac849f04b2a09f67d3404b176426c601f5db3c32fe1e87618fd0e915d6b430c7c04c1a'
